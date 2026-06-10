ecc4ac7220bc6a7aad2219f0e6deee0099b9b303
dnvxz9z5df-droid <dnvxz9z5df@privaterelay.appleid.com>
2026-06-10 15:25:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'edno-hub-v96'; // ← incrémenter à chaque déploiement (v93, v94...)
+const CACHE = 'edno-hub-v97'; // ← incrémenter à chaque déploiement (v93, v94...)
 const ASSETS = [
   '/Edno-hub/',
   '/Edno-hub/index.html',
